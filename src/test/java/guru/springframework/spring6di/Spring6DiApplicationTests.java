ab559ed88032c6d2dfa9e65a3a40a4a47bb1ce8c
Erick Dourado <erickdourado_@hotmail.com>
2025-04-25 16:44:15 -0300

    adding test context example

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,13 +1,29 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
+    @Autowired
+    ConfigurableApplicationContext context;
+
+    @Autowired
+    MyController myController;
+
     @Test
-    void contextLoads() {
+    void testAutowireOfController() {
+        System.out.println(myController.sayHello());
+    }
+
+    @Test
+    void testControllerFromCtx() {
+        MyController myController = context.getBean(MyController.class);
+        System.out.println(myController.sayHello());
     }
 
 }
